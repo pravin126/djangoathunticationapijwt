a5252280431c6e2c9fd930b8923706303d4ad2ec
pravin126 <pravinkalokhe5@gmail.com>
2023-09-09 18:54:29 +0530

    run on DRF

--- djnagoauth/account/views.py
+++ djnagoauth/account/views.py
@@ -1,3 +1,9 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+
+
+
+class UserRegistationView(APIView):
+    def post(self,request, format=None):
+        return Response({'msg':'Registation Success'})
